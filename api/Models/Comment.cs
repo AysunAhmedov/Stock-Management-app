5296c37bfef57765af63d30781c4a1c813ec908d
ays94 <icehack@abvbg>
2025-01-21 16:18:38 +0200

    Initial commit

--- api/Models/Comment.cs
+++ api/Models/Comment.cs
@@ -6,16 +6,23 @@ using System.Threading.Tasks;
 
 namespace api.Models
 {
-    [Table("Comments")]
     public class Comment
     {
-        public int Id { get; set; }
-        public string Title { get; set; } = string.Empty;
-        public string Content { get; set; } = string.Empty;
-        public DateTime CreatedOn { get; set; } = DateTime.Now;
-        public int? StockId { get; set; }
-        public Stock? Stock { get; set; }
-        public string AppUserId { get; set; }
-        public AppUser AppUser { get; set; }
+        public int Id { get; set; } 
+
+        public string Symbol { get; set; } = string.Empty;
+
+        public string CompanyName { get; set; } = string.Empty;
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal Purchase { get; set; }
+
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal LastDiv { get; set; }
+
+        public string Industry { get; set; } = string.Empty;
+
+        public long MarketCap { get; set; }
+
+        List<Comment> Comments { get; set; } = new List<Comment>();
     }
 }
