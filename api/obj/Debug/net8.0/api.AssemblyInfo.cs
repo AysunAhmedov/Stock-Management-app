a540498d50c1d510cfebae6cee08e6d21f44fcd8
ays94 <icehack@abvbg>
2025-01-22 17:36:16 +0200

    last commit for the day

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a00fd4b248079c5a62751b0ce88daa6b5eb43413")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8a6232fd320eb4c1fc08c701294e697602c48df8")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
